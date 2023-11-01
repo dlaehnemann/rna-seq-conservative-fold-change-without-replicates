d494b6d9ddfffde7ff60add11803417c7ee5d89d
dlaehnemann <david.laehnemann@hhu.de>
2023-11-01 14:54:53 +0100

    create rule to transform kallisto output to gfold input

---
+++ workflow/scripts/kallisto_quant_to_gfold_input.R
@@ -0,0 +1,19 @@
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+library(tidyverse)
+
+read_tsv(
+    str_c(snakemake@input[["kallist_folder"]], "abundance.tsv")
+) |>
+  select(
+    target_id,
+    target_id,
+    tpm,
+    eff_length
+  ) |>
+  write_tsv(
+    snakemake@output[["gfold_tsv"]],
+    col_names = FALSE
+  )
