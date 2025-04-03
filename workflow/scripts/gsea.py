7bd527a0c3b4dbe95bb422f398fcbd350759309f
David Laehnemann <david.laehnemann@hhu.de>
2025-04-03 20:01:00 +0200

    chore: black formatting on scripts

--- workflow/scripts/gsea.py
+++ workflow/scripts/gsea.py
@@ -1,30 +1,52 @@
 import sys
+
 sys.stderr = open(snakemake.log[0], "w")
 
 import pandas as pd
 import gseapy as gp
 
-background = pd.read_csv(snakemake.input.unfiltered, sep="\t").loc[:, "gene_symbol"].drop_duplicates()
+background = (
+    pd.read_csv(snakemake.input.unfiltered, sep="\t")
+    .loc[:, "gene_symbol"]
+    .drop_duplicates()
+)
 
-gfold_change = pd.read_csv(snakemake.input.filtered, sep="\t").loc[:, "gene_symbol"].drop_duplicates()
+gfold_change = (
+    pd.read_csv(snakemake.input.filtered, sep="\t")
+    .loc[:, "gene_symbol"]
+    .drop_duplicates()
+)
 
 if snakemake.wildcards.enrichr_library == "MSigDB_Hallmark_2020":
+
     def linkout_func(term):
         return f"https://www.gsea-msigdb.org/gsea/msigdb/cards/HALLMARK_{term.upper().replace(' ', '_')}"
-elif snakemake.wildcards.enrichr_library in ("GO_Biological_Process_2020", "GO_Molecular_Function_2021", "GO_Cellular_Component_2021"):
+
+elif snakemake.wildcards.enrichr_library in (
+    "GO_Biological_Process_2020",
+    "GO_Molecular_Function_2021",
+    "GO_Cellular_Component_2021",
+):
+
     def linkout_func(term):
         goid = term.split(" ")[-1].strip("()")
         return f"https://www.ebi.ac.uk/QuickGO/term/{goid}"
+
 else:
+
     def linkout_func(term):
         return f"https://www.google.com/search?q={term}"
 
 
 if len(gfold_change) <= 1:
     # enrichment does only work with at least 2 genes
-    pd.DataFrame(columns=["term", "linkout", "p-value", "FDR", "odds ratio", "genes"]).to_csv(snakemake.output[0], sep='\t', index=False)
+    pd.DataFrame(
+        columns=["term", "linkout", "p-value", "FDR", "odds ratio", "genes"]
+    ).to_csv(snakemake.output[0], sep="\t", index=False)
 else:
-    gene_set = gp.parser.download_library(snakemake.wildcards.enrichr_library, snakemake.params.species)
+    gene_set = gp.parser.download_library(
+        snakemake.wildcards.enrichr_library, snakemake.params.species
+    )
 
     enrichment = gp.enrich(
         gene_list=gfold_change,
@@ -37,39 +59,35 @@ else:
     dotplot = gp.dotplot(
         enrichment.results,
         column="Adjusted P-value",
-        x='Gene_set', # set x axis, so you could do a multi-sample/library comparsion
+        x="Gene_set",  # set x axis, so you could do a multi-sample/library comparsion
         size=10,
         top_term=top_n,
-        figsize=(16,top_n+2),
+        figsize=(16, top_n + 2),
         title=snakemake.wildcards.enrichr_library,
-        cmap='viridis',
-        xticklabels_rot=45, # rotate xtick labels
-        show_ring=True, # set to False to revmove outer ring
-        marker='o',
+        cmap="viridis",
+        xticklabels_rot=45,  # rotate xtick labels
+        show_ring=True,  # set to False to revmove outer ring
+        marker="o",
     ).get_figure()
 
     dotplot.tight_layout()
 
-    dotplot.savefig(
-        fname = snakemake.output["dotplot_top_n_genes"]
-    )
+    dotplot.savefig(fname=snakemake.output["dotplot_top_n_genes"])
 
     barplot = gp.barplot(
         enrichment.results,
         column="Adjusted P-value",
-        group='Gene_set', # set group, so you could do a multi-sample/library comparsion
+        group="Gene_set",  # set group, so you could do a multi-sample/library comparsion
         size=10,
         top_term=top_n,
-        figsize=(18,top_n+2),
+        figsize=(18, top_n + 2),
         title=snakemake.wildcards.enrichr_library,
-        cmap='Dark2',
+        cmap="Dark2",
     ).get_figure()
 
     barplot.tight_layout()
 
-    barplot.savefig(
-        fname = snakemake.output["barplot_top_n_genes"]
-    )
+    barplot.savefig(fname=snakemake.output["barplot_top_n_genes"])
 
     results = enrichment.results.sort_values("P-value")
     results.drop(columns="Gene_set", inplace=True)
@@ -82,4 +100,4 @@ else:
 
     results.insert(1, "linkout", results["term"].map(linkout_func))
 
-    results.to_csv(snakemake.output["tsv"], sep='\t', index=False)
+    results.to_csv(snakemake.output["tsv"], sep="\t", index=False)
